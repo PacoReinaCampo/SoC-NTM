e371d9227a21832193e6d66691cefa2830ba4d6a
PacoReinaCampo <frareicam@gmail.com>
2022-06-08 14:39:11 +0200

    Working in SoC-NTM

--- sim/vhdl/tests/design/math/calculus/vivado/simulate.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call ../../../../../../../settings64_vivado.bat
-
-xvhdl -prj system.prj
-xelab model_calculus_testbench
-xsim -R model_calculus_testbench
-pause
--- sim/vhdl/tests/model/controller/FNN/convolutional/vivado/simulate.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call ../../../../../../../../settings64_vivado.bat
-
-xvhdl -prj system.prj
-xelab model_convolutional_fnn_testbench
-xsim -R model_convolutional_fnn_testbench
-pause
